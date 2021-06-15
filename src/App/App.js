b75181a85746defe086758b2eb1210a4fd2ece9c
Matt Umland <mattumland@gmail.com>
2021-06-15 16:50:43 -0400

    Connect header to app

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
+import Header from '../Header/Header'
 
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <main className="App">
+      <Header />
+    </main>
   );
 }
 
