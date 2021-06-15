46a7b57b614994c0c02992269f9bd6b4a8001915
Matt Umland <mattumland@gmail.com>
2021-06-15 18:16:22 -0400

    Set up boiler plate

---
+++ src/CollectionList/CollectionList.js
@@ -0,0 +1,15 @@
+import './CollectionList.scss'
+
+const CollectionList = () => {
+
+    return (
+     <section className="collection-list">
+        <aside className="">
+            <h2>My Collections</h2>
+        </aside>
+
+     </section>   
+    )
+}
+
+export default CollectionList
