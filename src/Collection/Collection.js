1abba8122829a54ca5b59dbc63727fed212028dd
Matt Umland <mattumland@gmail.com>
2021-06-16 10:05:16 -0400

    Add state and props for controlled form

--- src/Collection/Collection.js
+++ src/Collection/Collection.js
@@ -2,15 +2,36 @@ import './Collection.scss'
 import pencil from '../icons/pencil.png'
 import bin from '../icons/bin.png'
 import check from '../icons/checkbox-checked.png'
+import { useState } from 'react'
 
-const Collection = ({ title }) => {
+const Collection = ({ title, removeCollection, addCollection }) => {
+
+    const [createMode, setCreateMode] = useState(title.length > 0)
+    const [formTitle, setFormTitle] = useState('add a title to your collection')
+
+    handleChange = (e) => {
+        setFormTitle(e.target.value)
+    }
+    
+    submitCollection = (e) => {
+        e.preventDefault();
+        
+    }
 
     return (
         <article className='collection'>
             <div className='title-container'>
                 <h3>{title}</h3>
             </div>
-            <form className='hidden'></form>
+            <form className='hidden'>
+                <input 
+                    type='text'
+                    placeHolder={title || formTitle}
+                    name='title'
+                    value={formTitle}
+                    onChange={event => handleChange(event)}
+                />
+            </form>
             <aside className='button-container'>
                 <button>
                     <img src={bin} alt='Delete icon: a trash can'/>
