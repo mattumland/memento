d1b2c1b06c0e019da268ee0db737323874dc5e7b
Matt Umland <mattumland@gmail.com>
2021-06-15 21:18:22 -0400

    Build initial collection container

--- src/CollectionList/CollectionList.js
+++ src/CollectionList/CollectionList.js
@@ -3,11 +3,11 @@ import './CollectionList.scss'
 const CollectionList = () => {
 
     return (
-     <section className="collection-list">
-        <aside className="">
+     <section className='collection-list'>
+        <aside className='collection-title'>
             <h2>My Collections</h2>
         </aside>
-
+        <button className='add-button'>+ Add New Collection</button>
      </section>   
     )
 }
