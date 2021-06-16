28b17c8b45f4f55801d3364d81191f75698925de
Matt Umland <mattumland@gmail.com>
2021-06-16 10:04:45 -0400

    Add methods for updating collections

--- src/App/App.js
+++ src/App/App.js
@@ -7,11 +7,25 @@ const App = () => {
 
   const [collectionData, setCollectionData] = useState(['Coins', 'World War II Stamps', 'Faberge Eggs'])
 
+  const removeCollection = (collectionName) => {
+    const newCollection = collectionData.filter(collection => collection !== collectionName)
+    setCollectionData(newCollection)
+  }
+
+  const addCollection = (newCollection) => {
+    setCollectionData( {collectionData: [...collectionData, newCollection]} )
+  }
+
+  const updateCollection = () => {
+  }
+
   return (
     <main className="App">
       <Header />
       <CollectionList 
         collectionData={collectionData}
+        removeCollection={removeCollection}
+        addCollection={addCollection}
       />
     </main>
   );
