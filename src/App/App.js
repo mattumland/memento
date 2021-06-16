ead87dfbf6a13603efb29dbdbe54a02bd060b567
Matt Umland <mattumland@gmail.com>
2021-06-15 20:45:00 -0400

    Hook up CollectionList in App

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import './App.scss'
 import Header from '../Header/Header'
+import CollectionList from '../CollectionList/CollectionList'
 
 const App = () => {
   return (
     <main className="App">
       <Header />
+      <CollectionList />
     </main>
   );
 }
