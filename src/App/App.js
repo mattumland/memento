637cc7458cfdfa4b4479a5ce25dac6072c90c7c4
Matt Umland <mattumland@gmail.com>
2021-06-15 22:23:42 -0400

    Add state and default data in app

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,18 @@
 import './App.scss'
 import Header from '../Header/Header'
 import CollectionList from '../CollectionList/CollectionList'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [collectionData, setCollectionData] = useState(['Faberge Eggs', 'World War II Stamps'])
+
   return (
     <main className="App">
       <Header />
-      <CollectionList />
+      <CollectionList 
+        collectionData={collectionData}
+      />
     </main>
   );
 }
