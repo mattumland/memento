f2bc5b766f4427ba98467ea8147ebe0ead751782
Matt Umland <mattumland@gmail.com>
2021-06-16 15:12:37 -0400

    Change state and update method to use ids for collections

--- src/App/App.js
+++ src/App/App.js
@@ -5,10 +5,10 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [collectionData, setCollectionData] = useState(['Coins', 'World War II Stamps', 'Faberge Eggs'])
+  const [collectionData, setCollectionData] = useState([{id: 1, title: 'Coins'}, {id: 2, title: 'World War II Stamps'}, {id:3, title: 'Faberge Eggs'}])
 
-  const removeCollection = (collectionName) => {
-    const newCollection = collectionData.filter(collection => collection !== collectionName)
+  const removeCollection = (id) => {
+    const newCollection = collectionData.filter(collection => collection.id !== id)
     setCollectionData(newCollection)
   }
 
@@ -16,9 +16,14 @@ const App = () => {
     setCollectionData( {collectionData: [...collectionData, newCollection]} )
   }
 
-  const updateCollection = (oldTitle, newTitle) => {
-    const index = collectionData.indexOf(oldTitle)
-    collectionData[index] = newTitle;
+  const updateCollection = (id, newTitle) => {
+    let collectionUpdate = collectionData
+    collectionUpdate.forEach(collection => {
+      if (collection.id === id) {
+        collection.title = newTitle
+      }
+    })
+    setCollectionData(collectionUpdate);
   }
 
   return (
