eaf86a9c34984b1304deb6853bbb96fc4020c249
Matt Umland <mattumland@gmail.com>
2021-06-16 15:13:04 -0400

    Add submit collection method to change state in app

--- src/Collection/Collection.js
+++ src/Collection/Collection.js
@@ -2,18 +2,12 @@ import './Collection.scss'
 import pencil from '../icons/pencil.png'
 import bin from '../icons/bin.png'
 import check from '../icons/checkbox-checked.png'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-const Collection = ({ title, removeCollection, addCollection }) => {
-
-    /*
-        conditional render form/title/buttons given createMode status 
-        add form styling
-
-    */
+const Collection = ({ title, id, removeCollection, updateCollection }) => {
 
     const [createMode, setCreateMode] = useState(!title.length > 0)
-    const [formTitle, setFormTitle] = useState('add a title to your collection')
+    const [formTitle, setFormTitle] = useState(title)
 
     const handleChange = (e) => {
         setFormTitle(e.target.value)
@@ -21,6 +15,8 @@ const Collection = ({ title, removeCollection, addCollection }) => {
     
    const submitCollection = (e) => {
         e.preventDefault();
+        updateCollection(id, formTitle)
+        toggleCreateMode();
     }
 
     const toggleCreateMode = () => {
@@ -34,25 +30,25 @@ const Collection = ({ title, removeCollection, addCollection }) => {
     return (
         <article className='collection'>
             <div className={createMode ? 'hidden' : 'title-container'}>
-                <h3>{title}</h3>
+                <h3>{formTitle}</h3>
             </div>
             <form className={createMode ? 'collection-form' : 'hidden'}>
                 <input 
                     type='text'
-                    placeholder={title || formTitle}
+                    placeholder={formTitle}
                     name='title'
                     value={formTitle}
                     onChange={event => handleChange(event)}
                 />
             </form>
             <aside className='button-container'>
-                <button onClick={e => removeCollection(title)}>
+                <button onClick={e => removeCollection(id)}>
                     <img src={bin} alt='Delete icon: a trash can'/>
                 </button>
                 <button onClick={e => toggleCreateMode()} className={createMode ? 'hidden' : ''}>
                     <img src={pencil} alt='Edit icon: a pencil' />
                 </button>
-                <button onClick={e => toggleCreateMode()} className={createMode ? '' : 'hidden'}>
+                <button onClick={e => submitCollection(e)} className={createMode ? '' : 'hidden'}>
                     <img src={check} alt='Complete icon: a checkmark' />
                 </button>
             </aside>
