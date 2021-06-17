37969dcf4073ce7d215e939f0a945423e64fc4b2
Matt Umland <mattumland@gmail.com>
2021-06-17 12:07:13 -0400

    Install cypress

---
+++ cypress/integration/memento.spec.js
@@ -0,0 +1,10 @@
+describe('Memento test suite', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000')
+    })
+
+    it('Should display the memento logo and title', () => {
+        
+    })
+
+})
